27b6b1dee74c879a0c1326df5edf3812a2b43871
LeHuynhThanhNgan0611 <nle007492@gmail.com>
2024-05-01 14:25:30 +0700

    update brand

--- src/main/java/com/codingwithn/shopapp/controller/BrandController.java
+++ src/main/java/com/codingwithn/shopapp/controller/BrandController.java
@@ -17,8 +17,8 @@ public class BrandController {
         // Thêm các thương hiệu vào danh sách
         brands.add(new Brand("1", "https://res.cloudinary.com/ddbvpbkql/image/upload/v1713274378/brand/nike.png", "Nike", 265, true));
         brands.add(new Brand("2", "https://res.cloudinary.com/ddbvpbkql/image/upload/v1713274378/brand/adidas.png", "Adidas", 95, true));
-        brands.add(new Brand("8", "https://res.cloudinary.com/ddbvpbkql/image/upload/v1713274378/brand/adidas.png", "Kenwood", 36, false));
-        brands.add(new Brand("9", "https://res.cloudinary.com/ddbvpbkql/image/upload/v1713274378/brand/adidas.png", "IKEA", 36, false));
+        brands.add(new Brand("8", "https://res.cloudinary.com/ddbvpbkql/image/upload/v1713274378/brand/kenwood.png", "Kenwood", 36, false));
+        brands.add(new Brand("9", "https://res.cloudinary.com/ddbvpbkql/image/upload/v1713274378/brand/ikea.png", "IKEA", 36, false));
         brands.add(new Brand("5", "https://res.cloudinary.com/ddbvpbkql/image/upload/v1713274378/brand/apple.png", "Apple", 16, true));
         brands.add(new Brand("10", "https://res.cloudinary.com/ddbvpbkql/image/upload/v1713274378/brand/acer.png", "Acer", 36, false));
         brands.add(new Brand("3", "https://res.cloudinary.com/ddbvpbkql/image/upload/v1713274378/brand/jordan.png", "Jordan", 36, true));
