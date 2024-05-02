7514672fb53010155a7730933c0a123bc6047814
LeHuynhThanhNgan0611 <nle007492@gmail.com>
2024-05-02 20:28:51 +0700

    update product category

--- src/main/java/com/codingwithn/shopapp/models/ProductCategory.java
+++ src/main/java/com/codingwithn/shopapp/models/ProductCategory.java
@@ -4,11 +4,22 @@ import java.util.List;
 
 public class ProductCategory {
     private String categoryId;
+    private String name;
+    private String image;
+    private String parentId;
+    private boolean featured;
     private List<Product> products;
 
-    public ProductCategory(String categoryId, List<Product> productList) {
+    public ProductCategory() {
+    }
+
+    public ProductCategory(String categoryId, String name, String image, String parentId, boolean featured, List<Product> products) {
         this.categoryId = categoryId;
-        this.products = productList;
+        this.name = name;
+        this.image = image;
+        this.parentId = parentId;
+        this.featured = featured;
+        this.products = products;
     }
 
     public String getCategoryId() {
@@ -19,11 +30,55 @@ public class ProductCategory {
         this.categoryId = categoryId;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(String parentId) {
+        this.parentId = parentId;
+    }
+
+    public boolean isFeatured() {
+        return featured;
+    }
+
+    public void setFeatured(boolean featured) {
+        this.featured = featured;
+    }
+
     public List<Product> getProducts() {
         return products;
     }
 
     public void setProducts(List<Product> products) {
         this.products = products;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductCategory{" +
+                "categoryId='" + categoryId + '\'' +
+                ", name='" + name + '\'' +
+                ", image='" + image + '\'' +
+                ", parentId='" + parentId + '\'' +
+                ", featured=" + featured +
+                ", products=" + products +
+                '}';
     }
 }
